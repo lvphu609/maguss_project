6ba54357c1584da36d9543df3bde3903fdeed52d
Le Vinh Phu <lvphu609@gmail.com>
2015-03-27 00:38:37 +0700

    source first commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/maguss_project/admin/');
+define('HTTP_CATALOG', 'http://localhost/maguss_project/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/maguss_project/admin/');
+define('HTTPS_CATALOG', 'http://localhost/maguss_project/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\maguss_project/admin/');
+define('DIR_SYSTEM', 'C:\wamp\www\maguss_project/system/');
+define('DIR_LANGUAGE', 'C:\wamp\www\maguss_project/admin/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\maguss_project/admin/view/template/');
+define('DIR_CONFIG', 'C:\wamp\www\maguss_project/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\maguss_project/image/');
+define('DIR_CACHE', 'C:\wamp\www\maguss_project/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\maguss_project/system/download/');
+define('DIR_UPLOAD', 'C:\wamp\www\maguss_project/system/upload/');
+define('DIR_LOGS', 'C:\wamp\www\maguss_project/system/logs/');
+define('DIR_MODIFICATION', 'C:\wamp\www\maguss_project/system/modification/');
+define('DIR_CATALOG', 'C:\wamp\www\maguss_project/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'maguss');
+define('DB_PREFIX', 'm_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/maguss_project/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/maguss_project/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\maguss_project/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\maguss_project/system/');
+define('DIR_LANGUAGE', 'C:\wamp\www\maguss_project/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\maguss_project/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\maguss_project/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\maguss_project/image/');
+define('DIR_CACHE', 'C:\wamp\www\maguss_project/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\maguss_project/system/download/');
+define('DIR_UPLOAD', 'C:\wamp\www\maguss_project/system/upload/');
+define('DIR_MODIFICATION', 'C:\wamp\www\maguss_project/system/modification/');
+define('DIR_LOGS', 'C:\wamp\www\maguss_project/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'maguss');
+define('DB_PREFIX', 'm_');
