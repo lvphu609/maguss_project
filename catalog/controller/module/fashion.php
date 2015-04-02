310b0ba4e16da66651dc294276997bf0512183f1
phucnguyen <hoangphuc.ag@gmail.com>
2015-04-02 16:52:02 +0700

    add Fashion module (home page)

---
+++ catalog/controller/module/fashion.php
@@ -0,0 +1,85 @@
+<?php
+class ControllerModuleFashion extends Controller {
+    public function index($setting) {
+        $data = array();
+
+        $this->load->model('tool/image');
+
+        if (isset($setting['title'])) {
+            $data['title'] = $setting['title'];
+        } else {
+            $data['title'] = '';
+        }
+
+        if (isset($setting['left_image'])) {
+            if ($setting['left_image']) {
+                $image = HTTP_SERVER . 'image/' . $setting['left_image'];
+            } else {
+                $image = $this->model_tool_image->resize('placeholder.png', 175, 250);
+            }
+            $data['left_image'] = $image;
+        } else {
+            $data['left_image'] = $this->model_tool_image->resize('placeholder.png', 175, 250);
+        }
+        if (isset($setting['left_text'])) {
+            $data['left_text'] = $setting['left_text'];
+        } else {
+            $data['left_text'] = '';
+        }
+
+        if (isset($setting['top_image'])) {
+            if ($setting['top_image']) {
+                $image = HTTP_SERVER . 'image/' . $setting['top_image'];
+            } else {
+                $image = $this->model_tool_image->resize('placeholder.png', 250, 350);
+            }
+            $data['top_image'] = $image;
+        } else {
+            $data['top_image'] = $this->model_tool_image->resize('placeholder.png', 250, 350);
+        }
+        if (isset($setting['top_text'])) {
+            $data['top_text'] = $setting['top_text'];
+        } else {
+            $data['top_text'] = '';
+        }
+
+        if (isset($setting['right_image'])) {
+            if ($setting['right_image']) {
+                $image = HTTP_SERVER . 'image/' . $setting['right_image'];
+            } else {
+                $image = $this->model_tool_image->resize('placeholder.png', 175, 250);
+            }
+            $data['right_image'] = $image;
+        } else {
+            $data['right_image'] = $this->model_tool_image->resize('placeholder.png', 175, 250);
+        }
+        if (isset($setting['right_text'])) {
+            $data['right_text'] = $setting['left_text'];
+        } else {
+            $data['right_text'] = '';
+        }
+
+        if (isset($setting['bottom_image'])) {
+            if ($setting['bottom_image']) {
+                $image = HTTP_SERVER . 'image/' . $setting['bottom_image'];
+            } else {
+                $image = $this->model_tool_image->resize('placeholder.png', 450, 250);
+            }
+            $data['bottom_image'] = $image;
+        } else {
+            $data['bottom_image'] = $this->model_tool_image->resize('placeholder.png', 450, 250);
+        }
+
+        if (isset($setting['bottom_text'])) {
+            $data['bottom_text'] = $setting['bottom_text'];
+        } else {
+            $data['bottom_text'] = '';
+        }
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/fashion.tpl')) {
+            return $this->load->view($this->config->get('config_template') . '/template/module/fashion.tpl', $data);
+        } else {
+            return $this->load->view('default/template/module/fashion.tpl', $data);
+        }
+    }
+}
---
+++ catalog/view/theme/default/template/module/fashion.tpl
@@ -0,0 +1,26 @@
+<div class="container pan-fashion" style="margin-top: 50px; margin-bottom: 25px;">
+    <div class="row">
+        <div class="col-md-3">
+            <img src="<?php echo $left_image; ?>" class="left-image">
+        </div>
+        <div class="col-md-3">
+            <h3 class="left-text"><?php echo $left_text; ?></h3>
+            <h3 class="bottom-text"><?php echo $bottom_text; ?></h3>
+        </div>
+        <div class="col-md-3">
+            <img src="<?php echo $top_image; ?>" class="top-image">
+        </div>
+        <div class="col-md-3">
+            <h3 class="top-text"><?php echo $top_text; ?></h3>
+            <h3 class="right-text"><?php echo $right_text; ?></h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-9">
+            <img src="<?php echo $bottom_image; ?>" class="bottom-image">
+        </div>
+        <div class="col-md-3">
+            <img src="<?php echo $right_image; ?>" class="right-image">
+        </div>
+    </div>
+</div>
