50165cf832899ba99e6a2d76513abe31a8a24e60
phucnguyen <hoangphuc.ag@gmail.com>
2015-04-14 18:00:53 +0700

    validate phone is number

--- catalog/controller/checkout/guest.php
+++ catalog/controller/checkout/guest.php
@@ -201,7 +201,7 @@ class ControllerCheckoutGuest extends Controller {
 				$json['error']['email'] = $this->language->get('error_email');
 			}
 
-			if ((utf8_strlen($this->request->post['telephone']) < 3) || (utf8_strlen($this->request->post['telephone']) > 32)) {
+			if ((utf8_strlen($this->request->post['telephone']) < 3) || !preg_match('/^[0-9]+$/', $this->request->post['telephone']) || (utf8_strlen($this->request->post['telephone']) > 32)) {
 				$json['error']['telephone'] = $this->language->get('error_telephone');
 			}
 
