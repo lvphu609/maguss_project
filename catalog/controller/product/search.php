0c08c615cbc9ba366d46b4fe16dcd106dfbc6167
Le Vinh Phu <lvphu609@gmail.com>
2015-04-16 01:50:23 +0700

    asdasdsad

--- catalog/controller/product/search.php
+++ catalog/controller/product/search.php
@@ -251,7 +251,7 @@ class ControllerProductSearch extends Controller {
 					foreach ($quantityDetail as $key => $value) {
 						if (count($value['images']) > 0) {
 							foreach ($value['images'] as $imgKey => $img) {
-								$quantityDetail[$key]['images'][$imgKey]['url'] = $this->model_tool_image->resize($img['name'], 250, 350);
+								$quantityDetail[$key]['images'][$imgKey]['url'] = $this->model_tool_image->resize($img['name'], 500, 700);
 							}
 						}
 					}
